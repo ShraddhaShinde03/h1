e7f2a42004afe51ad0df4ba581f3bab39b3a5ae8
Ram Agrawal <ramagrawal0610@gmail.com>
2022-10-05 19:34:25 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,30 @@
+package loops;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+//		int n=5;
+//		for(int i=0;i<n;i++) {
+//			for(int j=0;j<n-i-1;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0;j<=i;j++) {
+//				if(j==0||j==i)System.out.print("* ");
+//				else System.out.print("  ");
+//			}
+//			System.out.println();
+//		}
+//		for(int i=0;i<n-1;i++) {
+//			for(int j=0;j<=i;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0;j<n-i-1;j++) {
+//				if(j==0||j==n-i-2)System.out.print("* ");
+//				else System.out.print("  ");
+//			}
+//			System.out.println();
+//		}
+		System.out.println("    *\n   ***\n  * * *\n *  *  *\n*********\n *  *  *\n  * * *\n   ***\n    *");
+	}
+
+}
